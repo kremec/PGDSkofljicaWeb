33e1f52c7435bfd9ccc5d808663e875b748dfacc
kremec <klemen.remec@gmail.com>
2024-02-22 19:24:48 +0100

    Social media widget with Elfsight

--- src/assets/components/HomeContent.tsx
+++ src/assets/components/HomeContent.tsx
@@ -1,43 +1,48 @@
 import {Divider, Grid, Stack, Typography} from "@mui/material";
+import SocialMediaWidget from "./SocialMediaWidget.tsx";
 
 const HomeContent = () => {
-  return (
-      <div style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-      }}>
-          <Stack spacing={1} sx={{width: '90vw'}} display={"flex"} paddingY={3}>
-              <Grid container direction="row" spacing={2} textAlign={"center"}>
-                  <Grid item md={12} lg={6} textAlign={"left"}>
-                      <img src="/images/PGD_logo_znak.png" width="100px"/>
-                      <Typography fontWeight={"bold"} variant="h4">
-                          Prostovoljno<br/>
-                          Gasilsko<br/>
-                          Društvo<br/>
-                          ŠKOFLJICA
-                      </Typography>
-                      <Divider />
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <Stack spacing={1} sx={{width: '90vw'}} display={"flex"} paddingY={3}>
+                <Grid container direction="row" spacing={2} textAlign={"center"}>
+                    <Grid item md={12} lg={6} textAlign={"left"}>
+                        <img src="/images/PGD_logo_znak.png" width="100px"/>
+                        <Typography fontWeight={"bold"} variant="h4">
+                            Prostovoljno<br/>
+                            Gasilsko<br/>
+                            Društvo<br/>
+                            ŠKOFLJICA
+                        </Typography>
+                        <Divider/>
 
-                      <img src="/images/operativa.jpg" width="100%"/>
-                      <Typography variant="h5">
-                          O nas
-                      </Typography>
-                      <Typography>
-                          Smo društvo III. kategorije in osrednje društvo v občini Škofljica. V občini Škofljica deluje šest gasilskih društev, združenih v Gasilsko zvezo Škofljica.
-                          <br/><br/>
-                          PGD Škofljica je bilo ustanovljeno leta 1989 in je najstarejše društvo v občini. Delujemo na območju Škofljice in vasi Zalog, Gumnišče, Glinek, Gorenje Blato, Reber, Dole pri Škofljici in Pleše.
-                          <br/><br/>
-                          Posredujemo pri gašenju požarov, ob poplavah, neurjih in drugih nesrečah. Za uspešno posredovanje na intervencijah imamo gasilska vozila GV-1, GVM-1, GVC 16/25 in AC 16/50.
-                      </Typography>
-                  </Grid>
-                  <Grid item md={12} lg={6}>
-
-                  </Grid>
-              </Grid>
-          </Stack>
-      </div>
-  );
+                        <img src="/images/operativa.jpg" width="100%"/>
+                        <Typography variant="h5">
+                            O nas
+                        </Typography>
+                        <Typography>
+                            Smo društvo III. kategorije in osrednje društvo v občini Škofljica. V občini Škofljica
+                            deluje šest gasilskih društev, združenih v Gasilsko zvezo Škofljica.
+                            <br/><br/>
+                            PGD Škofljica je bilo ustanovljeno leta 1989 in je najstarejše društvo v občini. Delujemo na
+                            območju Škofljice in vasi Zalog, Gumnišče, Glinek, Gorenje Blato, Reber, Dole pri Škofljici
+                            in Pleše.
+                            <br/><br/>
+                            Posredujemo pri gašenju požarov, ob poplavah, neurjih in drugih nesrečah. Za uspešno
+                            posredovanje na intervencijah imamo gasilska vozila GV-1, GVM-1, GVC 16/25 in AC 16/50.
+                        </Typography>
+                    </Grid>
+                    <Grid item md={12} lg={6}>
+                        <SocialMediaWidget />
+                    </Grid>
+                </Grid>
+            </Stack>
+        </div>
+    );
 };
 
 export default HomeContent;
---
+++ src/assets/components/SocialMediaWidget.tsx
@@ -0,0 +1,51 @@
+import {useEffect} from "react";
+
+const HomeContent = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://static.elfsight.com/platform/platform.js';
+        script.setAttribute('data-use-service-core', '');
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            // Cleanup: remove the script when component unmounts
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    useEffect(() => {
+        function handleElementLoaded() {
+            const element = document.querySelector('a[href="https://elfsight.com/social-feed-widget/?utm_source=websites&utm_medium=clients&utm_content=social-feed&utm_term=localhost&utm_campaign=free-widget"]');
+            if (element) {
+                (element as HTMLElement).style.display = "none";
+            }
+        }
+        function checkElement() {
+            const element = document.querySelector('a[href="https://elfsight.com/social-feed-widget/?utm_source=websites&utm_medium=clients&utm_content=social-feed&utm_term=localhost&utm_campaign=free-widget"]');
+            if (element) {
+                handleElementLoaded();
+                clearInterval(intervalId);
+            }
+        }
+
+        if (document.readyState === 'complete') {
+            checkElement();
+        } else {
+            // If the DOM is not yet loaded, add an event listener
+            document.addEventListener('DOMContentLoaded', checkElement);
+        }
+        const intervalId = setInterval(checkElement, 100);
+
+        return () => {
+            document.removeEventListener('DOMContentLoaded', checkElement);
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div className="elfsight-app-2559c59f-8f5c-4de5-b360-70018ef9f66c" data-elfsight-app-lazy style={{}}></div>
+    );
+};
+
+export default HomeContent;
