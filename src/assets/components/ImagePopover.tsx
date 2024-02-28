962ffb310e58e804f9b1e0d1f7f7ecf1ce67ae90
kremec <klemen.remec@gmail.com>
2024-02-28 08:51:56 +0100

    Revert to previous ImagePopover

---
+++ src/assets/components/ImagePopover.tsx
@@ -0,0 +1,48 @@
+import {useState} from "react";
+import {Box, Dialog, DialogContent, DialogTitle} from "@mui/material";
+
+interface Props {
+    src: string;
+    alt: string;
+    width: string;
+}
+
+const ImagePopover = ({src, alt, width}: Props) => {
+    const handleOpen = () => {
+        setOpenPopover(true)
+    };
+    const handleClose = () => {
+        setOpenPopover(false)
+    };
+
+    const [openPopover, setOpenPopover] = useState(false);
+
+    return (
+        <>
+            <Box
+                width={width}
+                component="img"
+                alt={alt}
+                src={src}
+                onClick={handleOpen}
+            />
+            <Dialog
+                fullWidth={true}
+                maxWidth={false}
+                open={openPopover}
+                onClose={handleClose}
+                onClick={handleClose}
+            >
+                <DialogTitle>{alt}</DialogTitle>
+                <Box
+                    component="img"
+                    alt={alt}
+                    src={src}
+                    width={"100%"}
+                />
+            </Dialog>
+        </>
+    );
+};
+
+export default ImagePopover;
