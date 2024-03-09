31c75d67ca5426d410f965f90acbf2f25b4ae2c7
kremec <klemen.remec@gmail.com>
2024-03-09 16:09:37 +0100

    Added new GVC

--- src/assets/components/VehiclesContent.tsx
+++ src/assets/components/VehiclesContent.tsx
@@ -14,62 +14,41 @@ const HomeContent = () => {
                 </Typography>
                 <Divider/>
                 <Grid container direction="row" textAlign={"center"}>
+
                     <Grid item md={12} lg={6} padding={1}>
                         <Card>
                             <CardMedia component="div"
                                        sx={{objectFit: "contain"}}>
-                                <ModalImage small={"/images/vozniPark/ac_1.jpg"} large={"/images/vozniPark/ac_1.jpg"} alt={"AC"}/>
+                                <ModalImage small={"/images/vozniPark/gvc_2.jpg"} large={"/images/vozniPark/gvc_2.jpg"} alt={"Gasilsko vozilo s cisterno (GVC)"}/>
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="div">
-                                    AC
+                                    Gasilsko vozilo s cisterno (GVC)
                                 </Typography>
                                 <Typography color="text.secondary">
-                                    Podvozje: Mercedes Benz 1329 (2006)<br/>
-                                    Nadgradnja: Elektro Turnšek (2007) / Rosenbauer (2023)<br/>
-                                    Posadka vozila: 1 + 2<br/>
-                                    Črpalka: Turboinštitut - 1600 l/min<br/>
-                                    Količina vode: 6300 litrov<br/>
-                                    Vozilo je zaradi večje količine vode namenjeno oskrbi požarišča z vodo in prevozu
-                                    pitne vode.
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item md={12} lg={6} padding={1}>
-                        <Card>
-                            <CardMedia component="div"
-                                       sx={{objectFit: "contain"}}>
-                                <ModalImage small={"/images/vozniPark/gvc_1.jpg"} large={"/images/vozniPark/gvc_1.jpg"} alt={"GVC 16/25"}/>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    GVC 16/25
-                                </Typography>
-                                <Typography color="text.secondary">
-                                    Podvozje: Marcedes Benz 1324 (1992)<br/>
-                                    Nadgradnja: Gasilska vozila Pušnik (1997)<br/>
-                                    Posadka vozila: 5+1<br/>
-                                    Črpalka: Rosenbauer NH20 - 2000 l/min<br/>
-                                    Količina vode: 3400 litrov<br/>
-                                    Vozilo ima dva hitronapadalna navijaka.<br/>
+                                    Podvozje: MAN 13.290 (2021)<br/>
+                                    Nadgradnja: Wiss (2023)<br/>
+                                    Posadka vozila: 1 + 7<br/>
+                                    Črpalka: Ruberg - 2500 l/min<br/>
+                                    Prostornina rezervoarja: 2600 litrov<br/>
                                     Vozilo je namenjeno gašenju in reševanju pri požarih in manjših tehničnih
                                     intervencijah.
                                     Značilnosti vozila so večja količina vode v rezervoarju in možnosti izvedbe hitrega
-                                    napada
+                                    napada.
                                 </Typography>
                             </CardContent>
                         </Card>
                     </Grid>
+
                     <Grid item md={12} lg={6} padding={1}>
                         <Card>
                             <CardMedia component="div"
                                        sx={{objectFit: "contain"}}>
-                                <ModalImage small={"/images/vozniPark/gvm_1.jpg"} large={"/images/vozniPark/gvm_1.jpg"} alt={"GVM 1"}/>
+                                <ModalImage small={"/images/vozniPark/gvm_1.jpg"} large={"/images/vozniPark/gvm_1.jpg"} alt={"Gasilsko vozilo za moštvo (GVM)"}/>
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="div">
-                                    GVM-1
+                                    Gasilsko vozilo za moštvo (GVM)
                                 </Typography>
                                 <Typography color="text.secondary">
                                     Podvozje: Volkswagen Transporter (2016)<br/>
@@ -80,18 +59,42 @@ const HomeContent = () => {
                             </CardContent>
                         </Card>
                     </Grid>
+
                     <Grid item md={12} lg={6} padding={1}>
                         <Card>
                             <CardMedia component="div"
                                        sx={{objectFit: "contain"}}>
-                                <ModalImage small={"/images/vozniPark/gv_1.jpg"} large={"/images/vozniPark/gv_1.jpg"} alt={"GV 1"}/>
+                                <ModalImage small={"/images/vozniPark/ac_1.jpg"} large={"/images/vozniPark/ac_1.jpg"} alt={"Avtocisterna (AC)"}/>
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="div">
-                                    GV-1
+                                    Avtocisterna (AC)
                                 </Typography>
                                 <Typography color="text.secondary">
-                                    Podvozje: Mercedes Benz 313 Sprinter (2001)<br/>
+                                    Podvozje: Mercedes Benz 1329 (2006)<br/>
+                                    Nadgradnja: Elektro Turnšek (2007) / Rosenbauer (2023)<br/>
+                                    Posadka vozila: 1 + 2<br/>
+                                    Črpalka: Turboinštitut - 1600 l/min<br/>
+                                    Prostornina rezervoarja: 6300 litrov<br/>
+                                    Vozilo je zaradi večje količine vode namenjeno oskrbi požarišča z vodo in prevozu
+                                    pitne vode.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item md={12} lg={6} padding={1}>
+                        <Card>
+                            <CardMedia component="div"
+                                       sx={{objectFit: "contain"}}>
+                                <ModalImage small={"/images/vozniPark/gv_1.jpg"} large={"/images/vozniPark/gv_1.jpg"} alt={"Orodno gasilsko vozilo (GV)"}/>
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Orodno gasilsko vozilo (GV)
+                                </Typography>
+                                <Typography color="text.secondary">
+                                    Podvozje: Mercedes Benz Sprinter 313 (2001)<br/>
                                     Nadgradnja: Gasilska vozila Pušnik (2001)<br/>
                                     Posadka vozila: 8+1<br/>
                                     Črpalka: Prenosna motorna brizgalna Rosenbauer 8/8<br/>
@@ -101,6 +104,33 @@ const HomeContent = () => {
                             </CardContent>
                         </Card>
                     </Grid>
+
+                    <Grid item md={12} lg={6} padding={1}>
+                        <Card>
+                            <CardMedia component="div"
+                                       sx={{objectFit: "contain"}}>
+                                <ModalImage small={"/images/vozniPark/gvc_1.jpg"} large={"/images/vozniPark/gvc_1.jpg"} alt={"Gasilsko vozilo s cisterno (GVC)"}/>
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Gasilsko vozilo s cisterno (GVC)
+                                </Typography>
+                                <Typography color="text.secondary">
+                                    Podvozje: Marcedes Benz 1324 (1992)<br/>
+                                    Nadgradnja: Gasilska vozila Pušnik (1997)<br/>
+                                    Posadka vozila: 5+1<br/>
+                                    Črpalka: Rosenbauer NH20 - 2000 l/min<br/>
+                                    Prostornina rezervoarja: 3400 litrov<br/>
+                                    Vozilo ima dva hitronapadalna navijaka.<br/>
+                                    Vozilo je namenjeno gašenju in reševanju pri požarih in manjših tehničnih
+                                    intervencijah.
+                                    Značilnosti vozila so večja količina vode v rezervoarju in možnosti izvedbe hitrega
+                                    napada.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
                 </Grid>
             </Stack>
         </div>
