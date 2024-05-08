ae61c4205015f63cdcc1b267796c08605fa10de7
kremec <klemen.remec@gmail.com>
2024-05-08 11:01:13 +0200

    Fixing JuicerFeed #4

--- src/assets/components/SocialMediaWidget.tsx
+++ src/assets/components/SocialMediaWidget.tsx
@@ -19,54 +19,37 @@ const SocialMediaWidget = () => {
             }
         };
     }, []);
-    // useEffect(() => {
-    //     const script = document.createElement('script');
-    //     script.src = 'https://static.elfsight.com/platform/platform.js';
-    //     script.setAttribute('data-use-service-core', '');
-    //     script.async = true;
-    //     document.body.appendChild(script);
-
-    //     return () => {
-    //         // Cleanup: remove the script when component unmounts
-    //         document.body.removeChild(script);
-    //     };
-    // }, []);
 
     // Remove watermark
-    // useEffect(() => {
-    //     function handleElementLoaded(element: Element) {
+    useEffect(() => {
+        function handleElementLoaded(element: Element) {
 
-    //         if (element) {
-    //             (element as HTMLElement).style.display = "none";
-    //         }
-    //     }
-    //     function checkElement() {
-    //         const elementWww = document.querySelector('a[href="https://elfsight.com/social-feed-widget/?utm_source=websites&utm_medium=clients&utm_content=social-feed&utm_term=www.pgd-skofljica.si&utm_campaign=free-widget"]');
-    //         const elementNoWww = document.querySelector('a[href="https://elfsight.com/social-feed-widget/?utm_source=websites&utm_medium=clients&utm_content=social-feed&utm_term=pgd-skofljica.si&utm_campaign=free-widget"]');
+            if (element) {
+                (element as HTMLElement).style.display = "none";
+            }
+        }
+        function checkElement() {
+            const element = document.querySelector('h1[class="referral"]');
 
-    //         if (elementWww) {
-    //             handleElementLoaded(elementWww);
-    //             clearInterval(intervalId);
-    //         }
-    //         if (elementNoWww) {
-    //             handleElementLoaded(elementNoWww);
-    //             clearInterval(intervalId);
-    //         }
-    //     }
+            if (element) {
+                handleElementLoaded(element);
+                clearInterval(intervalId);
+            }
+        }
 
-    //     if (document.readyState === 'complete') {
-    //         checkElement();
-    //     } else {
-    //         // If the DOM is not yet loaded, add an event listener
-    //         document.addEventListener('DOMContentLoaded', checkElement);
-    //     }
-    //     const intervalId = setInterval(checkElement, 100);
+        if (document.readyState === 'complete') {
+            checkElement();
+        } else {
+            // If the DOM is not yet loaded, add an event listener
+            document.addEventListener('DOMContentLoaded', checkElement);
+        }
+        const intervalId = setInterval(checkElement, 100);
 
-    //     return () => {
-    //         document.removeEventListener('DOMContentLoaded', checkElement);
-    //         clearInterval(intervalId);
-    //     };
-    // }, []);
+        return () => {
+            document.removeEventListener('DOMContentLoaded', checkElement);
+            clearInterval(intervalId);
+        };
+    }, []);
 
     return (
         //<div className="elfsight-app-2559c59f-8f5c-4de5-b360-70018ef9f66c data-elfsight-app-lazy"></div>
